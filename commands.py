73c41c650e0d98947e47c9cbc4a2fec960ccc10f
Razil <kontra9797@mail.ru>
2024-10-25 18:03:40 +0300

    Сделал кнопки постоянными(старт, меню, о нас, адреса доставок) По какой то причине кнопки иногда пропадают , нужно пересмотреть функции

--- commands.py
+++ commands.py
@@ -13,22 +13,45 @@ def botCommands():
         bot.send_message(message.chat.id, ' Мы крупная федеральная сеть доставки вкуснейших суши и роллов"Koldun-Boldun"\n Мы занимаемся этим делом с 2015 года и готовы предложить вам продукцию из свежайших ингридиентов\n У нас вы можете заказать как через данного бота так и по телефону +7 999 999 99 99 ' )
 
     @bot.message_handler(commands = ["start"])
-    def main(message):
-        bot.send_message(message.chat.id, f"Привет, {message.from_user.first_name}, жмакай понравившуюся кнопку")
+    def start(message):
+        markup = types.ReplyKeyboardMarkup()
+        butn1 = types.KeyboardButton('О нас')
+        butn2 = types.KeyboardButton('Адреса доставок')
+        butn3 = types.KeyboardButton('Меню')
+        butn4 = types.KeyboardButton('Обратная связь')
+
+        markup.row(butn1, butn2, butn3, butn4)
+        reply_markup=markup
+        bot.register_next_step_handler(message, on_click)
+    def on_click(message):
+        if message.text == 'О нас':
+            about(message)
+
+        elif message.text == 'Адреса доставок':
+            adress(message)
+
+        elif message.text == 'Меню':
+            menu1(message)
+
+        elif message.text == 'Обратная связь':
+            feedback(message)
+        bot.register_next_step_handler(message, start)
+
 
     @bot.message_handler(commands=["adress"])
-    def main(message):
+    def adress(message):
         bot.send_message(message.chat.id, f"Мы осуществляем доставки по:\n Мурино - стоимость доставки 300 рублей(при заказе от 2000 бесплатно)\n Пранас - стоимость доставки 400 рублей(при заказе от 2000 бесплатно)\n Кудрово - стоимость доставки 500 рублей")
 
     @bot.message_handler(commands=["menu"])
-    def main(message):
+    def menu1(message):
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton('Роллы', callback_data = 'rolls'))
-        markup.add(types.InlineKeyboardButton('Пицца', callback_data='pizza'))
-        markup.add(types.InlineKeyboardButton('Напитки', callback_data='drinks'))
-        markup.add(types.InlineKeyboardButton('Соусы', callback_data='sause'))
+        btn1 = types.InlineKeyboardButton('Роллы', callback_data = 'rolls')
+        btn2 = types.InlineKeyboardButton('Пицца', callback_data='pizza')
+        btn3 = types.InlineKeyboardButton('Напитки', callback_data='drinks')
+        btn4 = types.InlineKeyboardButton('Соусы', callback_data='sause')
 
-        bot.reply_to(message, "Выберите раздел :" , reply_markup = markup)
+        markup.row(btn1,btn2,btn3,btn4)
+        bot.reply_to(message, "Выбери нужный раздел",reply_markup = markup)
     @bot.callback_query_handler(func=lambda callback: True)
     def callback_message(callback):
         if callback.data == 'rolls':
@@ -42,7 +65,7 @@ def botCommands():
 
 
     @bot.message_handler(commands=["feedback"])
-    def main(message):
+    def feedback(message):
         bot.send_message(message.chat.id, "Если вам не довезли заказ, нагрубили , вам не понравилась еда или вы хотите оставить хороший отзыв можете сделать это в чате с нашим директором - @otbornyi")
 
     @bot.message_handler()
